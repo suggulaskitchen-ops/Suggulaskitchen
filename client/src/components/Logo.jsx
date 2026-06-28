661b4f999eb03aa4e25d71c6e98f5826dc76fccf
arteja.09@gmail.com <arteja.09@gmail.com>
2026-06-28 22:50:34 +0530

    logo correction

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,5 +1,5 @@
 function Logo({ className, alt = "Suggula's Kitchen" }) {
-  const src = import.meta.env.BASE_URL + 'images/suggula-logo.svg'
+  const src = import.meta.env.BASE_URL + 'images/image.png'
   return <img src={src} alt={alt} className={className} />
 }
 
