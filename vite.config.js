460c096e950449251b0f7a0c46e02f0c88f3a9ac
arteja.09@gmail.com <arteja.09@gmail.com>
2026-06-28 19:53:45 +0530

    image update

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   root: 'client',
-  server: {
-    port: 5173,
-    strictPort: true
-  },
+  base: '/Suggulaskitchen/',
   build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  },
-  plugins: [react()]
+    outDir: '../dist'
+  }
 })
